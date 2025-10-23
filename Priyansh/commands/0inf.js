1898e5a20a2c88ffb2bcbe16dd9517bb69f96467
Rahul-Bot-xd <raj91755623@gmail.com>
2025-10-23 16:27:50 +0300

    Update 0inf.js

--- Priyansh/commands/0inf.js
+++ Priyansh/commands/0inf.js
@@ -2,7 +2,7 @@ module.exports.config = {
 	name: "info",
 	version: "1.0.1", 
 	hasPermssion: 0,
-	credits: "𝐏𝐫𝐢𝐲𝐚𝐧𝐬𝐡 𝐑𝐚𝐣𝐩𝐮𝐭",
+	credits: "SARDAR RDX",
 	description: "Admin and Bot info.",
 	commandCategory: "...",
 	cooldowns: 1,
@@ -24,39 +24,31 @@ const time = process.uptime(),
 const moment = require("moment-timezone");
 var juswa = moment.tz("Asia/Dhaka").format("『D/MM/YYYY』 【HH:mm:ss】");
 var link =                                     
-["https://i.imgur.com/eDbdlvd.jpg"];
-var callback = () => api.sendMessage({body:` ╾━╤デ╦︻(▀̿Ĺ̯▀̿ ̿)🇮🇳 𝐀𝐃𝐌𝐈𝐍 𝐀𝐍𝐃 𝐁𝐎𝐓 𝐈𝐍𝐅𝐎𝐑𝐌𝐀𝐓𝐈𝐎𝐍 🇮🇳 
-(⌐▀͡ ̯ʖ▀)︻̷┻̿═━一-
+["https://i.imgur.com/Kj2CmiZ.jpegv"];
+var callback = () => api.sendMessage({body:`
+╔══✦──────────✦══╗
+          👑 𝐑𝐎𝐘𝐀𝐋 𝐁𝐎𝐓 𝐂𝐀𝐑𝐃 👑
+╚══✦──────────✦══╝
 
-☄️Bot Name︎︎︎☄️  ${global.config.BOTNAME}
+🌟 Bot Name   : ${global.config.BOTNAME}  
+👑 Owner      : 𝐑𝐀𝐇𝐔𝐋 𝐒𝐈𝐍𝐆𝐇 
+🔔 Prefix     : ${global.config.PREFIX}  
 
-🔥Bot Admin🔥☞︎︎︎☜︎︎︎✰ ℙ𝕣𝕚𝕪𝕒𝕟𝕤𝕙💔🥀
+✦────────────────✦
+📘 Facebook  → https://www.facebook.com/ve.ified.j.649774 
+📷 Instagram → instagram.com/babu9444  
+💬 Telegram  → Rahul support  
+📧 Email     → support@rahul fake  
+✦────────────────✦
 
-🙈bot andmin owner facebook id link🙈➪ www.facebook.com/priyanshu.rajput.official 💞🕊️
+📅 Date   : ${juswa}  
+⏳ Uptime : ${hours}:${minutes}:${seconds}  
 
-👋For Any Kind Of Help Contact On Telegram  Username 👉 @Priyanshrajput😇
-
-✧══════•❁❀❁•══════✧
-
-🌸Bot Prefix🌸☞︎︎︎☜︎︎︎✰ ${global.config.PREFIX}
-
-♥️Bot Owner♥️ ☞︎︎︎☜︎︎︎✰ 𝐏𝐫𝐢𝐲𝐚𝐧𝐬𝐡 𝐑𝐚𝐣𝐩𝐮𝐭
-
-🥳UPTIME🥳
-
-🌪️Today is🌪️ ☞︎︎︎☜︎︎︎✰ ${juswa} 
-
-⚡Bot is running⚡ ${hours}:${minutes}:${seconds}.
-
-✅Thanks for using ${global.config.BOTNAME} Bot🖤
-
-
-🦢🍒•••ꞪɛᏒɛ ɪʂ ɮ❍┼ ❍ωɳɜɽ ɳaʍɜ•••🌷💞
-┏━🕊️━━°❀•°:🎀🧸💙🧸🎀:°•❀°━━💞━┓
-🌸✦✧✧✧✧✰🍒ℙ𝕣𝕚𝕪𝕒𝕟𝕤𝕙🌿✰✧✧✧✧✦🌸
-┗━🕊️━━°❀•°:🎀🧸💙🧸🎀:°•❀°━━💞━┛
+✦────────────────✦
+✨ Powered with Love ❤️ by 𝐑𝐀𝐇𝐔𝐋✨
 
 
 `,attachment: fs.createReadStream(__dirname + "/cache/juswa.jpg")}, event.threadID, () => fs.unlinkSync(__dirname + "/cache/juswa.jpg")); 
       return request(encodeURI(link[Math.floor(Math.random() * link.length)])).pipe(fs.createWriteStream(__dirname+"/cache/juswa.jpg")).on("close",() => callback());
    };
+   
