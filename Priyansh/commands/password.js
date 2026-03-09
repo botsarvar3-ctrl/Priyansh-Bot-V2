8bc6eeda6f7fb5f31d3dd65e6130f66a28d54e0d
𝗬ᴀᴍ𝐃ʜᴜᴅ⚜️𓆪 <botsarvar3@gmail.com>
2026-03-09 10:00:28 +0530

    Update password.js

--- Priyansh/commands/password.js
+++ Priyansh/commands/password.js
@@ -2,7 +2,7 @@ module.exports.config = {
   name: "password",
   version: "1.0.0",
   hasPermssion: 0,
-  credits: "𝐏𝐫𝐢𝐲𝐚𝐧𝐬𝐡 𝐑𝐚𝐣𝐩𝐮𝐭",
+  credits: "𝐲𝐚𝐦𝐝𝐡𝐮𝐝",
   description: "",
   commandCategory: "GAME",
   usages: "[text 1] | [text 2]",
